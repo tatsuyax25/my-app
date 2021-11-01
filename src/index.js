411507b3ce048ce9ceb1ed559e0259279727e5f8
Miguel Angel Urena <urena_miguel82@outlook.com>
2021-10-31 21:48:41 -0700

    New Update

--- src/index.js
+++ src/index.js
@@ -3,23 +3,60 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Board extends React.Component {
-    renderSquare(i) { // In Board's renderSquare method, change the code to pass a prop called
-        return <Square value={i} />; // value to the Square
-    }
-}
-
-class Square extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: null,
+            squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
-    render() {
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+    renderSquare(i) {
         return (
-            <button className='square' onClick={function() { console.log('click'); }}>
-                {this.props.value}
-            </button>
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
         );
     }
+    render() {
+        const status = 'Next player: ' + (this.state.xIsNext
+    ? 'X' : 'O');
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+function Square(props) {
+        return (
+            <button className="square" onClick={props.onClick}>
+                {props.value}
+            </button>
+        );
 }
